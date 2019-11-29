6cf3c643f03f49b18a6304c8d2f4a9ebba1c9d47
Thundelly <thundelly0609@gmail.com>
2019-11-28 16:46:58 -0800

    Update 0.2 -- getRemainingDeck()

--- src/main/java/com/jeprod/Player.java
+++ src/main/java/com/jeprod/Player.java
@@ -3,5 +3,24 @@ package com.jeprod;
 import java.util.Vector;
 
 class Player {
-    Vector<Card> hand = new Vector<>(5);
+    private Vector<Card> _hand = new Vector<>(5);
+    private float _money;
+
+    private void addToHand(Card card) {
+        _hand.add(card);
+    }
+
+    Vector<Card> getHand() {
+        return _hand;
+    }
+
+    private void setMoney(float change) {
+        _money += change;
+    }
+
+    float getMoney() {
+        return _money;
+    }
+
+    
 }
